bce79b72f504f54e23a8d3af4b9d78388b9876a3
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2021-08-20 12:58:52 -0300

    ensure call Encrypter with correct values

--- src/domain/usecase/auth-usecase.spec.js
+++ src/domain/usecase/auth-usecase.spec.js
@@ -4,16 +4,25 @@ const AuthUseCase = require('./auth-usecase')
 
 
 const makeSut = () => {
+    class EncrypterSpy {
+        async compare(password, hashedpassword) {
+            this.password = password
+            this.hashedpassword = hashedpassword
+        }
+    }
     class LoadUserByEmailRepositorySpy {
         async load(email) {
             this.email = email
             return this.user
         }
     }
+    const encrypterSpy = new EncrypterSpy()
     const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
-    loadUserByEmailRepositorySpy.user = {}
-    const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
-    return { sut, loadUserByEmailRepositorySpy }
+    loadUserByEmailRepositorySpy.user = {
+        password: "hashed_password"
+    }
+    const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy)
+    return { sut, loadUserByEmailRepositorySpy, encrypterSpy}
 }
 
 describe('Auth UseCase', () => {
@@ -52,11 +61,18 @@ describe('Auth UseCase', () => {
         loadUserByEmailRepositorySpy.user = null
         const accessToken = await sut.auth('invalid_email@mail.com', 'any_password')
         expect(accessToken).toBeNull()
-    }) 
+    })
 
     it('Should return null if an invalid password is provided', async () => {
         const { sut } = makeSut()
         const accessToken = await sut.auth('valid_email@mail.com', 'invalid_password')
         expect(accessToken).toBeNull()
-    }) 
+    })
+
+    it('Should call Encrypter with correct values', async () => {
+        const { sut, loadUserByEmailRepositorySpy, encrypterSpy } = makeSut()
+        await sut.auth('valid_email@mail.com', 'any_password')
+        expect(encrypterSpy.password).toBe('any_password')
+        expect(encrypterSpy.hashedpassword).toBe(loadUserByEmailRepositorySpy.user.password)
+    })
 })
